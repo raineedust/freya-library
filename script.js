726e7bb0538b642301716cf4dce13df88fc7dd04
Tan Jin Yi <rayneedust@gmail.com>
2022-07-04 16:12:53 +0800

    Add form reset

--- script.js
+++ script.js
@@ -21,6 +21,7 @@ const addBook = document.querySelector("#add-book");
 function addToLibrary(title, author, pages, readStatus) {
   const book = new Book(title, author, pages, readStatus);
   myLibrary.push(book);
+  displayBook();
   console.log(book);
   console.log(myLibrary);
 }
@@ -30,7 +31,11 @@ function formVal() {
   const author = document.querySelector("#author").value;
   const pages = document.querySelector("#pages").value;
 
-  if (title !== "" && author !== "" && pages.match(/[^1-9]/)) {
+  if (
+    title !== "" &&
+    author !== "" &&
+    pages.match(/^([1-9][0-9][0-9]?|1000)$/)
+  ) {
     if (document.querySelector("#readStatus").checked) {
       const readStatus = "read";
       addToLibrary(title, author, pages, readStatus);
@@ -38,6 +43,7 @@ function formVal() {
       const readStatus = "unread";
       addToLibrary(title, author, pages, readStatus);
     }
+    document.querySelector("form").reset();
   } else {
     alert("Please enter the details correctly!");
   }
@@ -46,3 +52,20 @@ function formVal() {
 addBook.addEventListener("click", () => {
   formVal();
 });
+
+// Display UI
+
+function displayBook() {
+  const table = document.querySelector(".table");
+  const row = table.insertRow(1);
+  const cell1 = row.insertCell(0);
+  const cell2 = row.insertCell(1);
+  const cell3 = row.insertCell(2);
+  const cell4 = row.insertCell(3);
+  cell1.textContent = "NEW CELL1";
+  cell2.textContent = "NEW CELL2";
+  cell3.textContent = "NEW CELL3";
+  cell4.textContent = "NEW CELL4";
+}
+
+myLibrary.forEach(displayBook);
